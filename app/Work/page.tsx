093813c80b5bcf8033a557a633c5acea2c0f1e6e
ebmalabuyo <elijahbmalabuyo@gmail.com>
2024-03-09 22:17:14 -0800

    work page finished

--- app/Work/page.tsx
+++ app/Work/page.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
 
+import WorkCard from '@/components/WorkCard'
+
+//data 
+import work from "@/data/work.json"
+
 export default function Work() {
   return (
-    <div>Work</div>
+    <div className="mt-36 md:mt-64 mb-36 md:flex md:justify-center">
+      <div className="w-full lg:w-3/4 max-w-6xl md:p-2">
+
+
+      <section className='p-4'>
+          <div className='text-center ipadmini:text-left'>
+                    <h1 className='font-extrabold text-2xl ipadmini:text-4xl '>My Work</h1>
+                    <p>{"Work and projects I've contributed to"}</p>
+          </div>
+
+          <div className="flex flex-col gap-6 mt-6 md:p-8">
+                {work.map(each => {
+                          return (
+                              <div key={each.title}>
+                                <WorkCard {...each} />
+                              </div>
+                          )
+                        })}
+          </div>
+        </section>
+
+
+        </div>
+      </div>
   )
 }
