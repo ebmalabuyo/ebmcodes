89afac383f7f7c66cd7942c691d5d42a8d40fd47
ebmalabuyo <elijahbmalabuyo@gmail.com>
2024-03-10 15:38:08 -0700

    edits to grid sizing and technology page

--- app/Bookshelf/page.tsx
+++ app/Bookshelf/page.tsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function Bookshelf() {
   return (
-    <div>Bookshelf</div>
+    <div className="mt-36 md:mt-48 mb-36 md:flex md:justify-center">
+    <div className="w-full lg:w-3/4 max-w-6xl md:p-2">
+      
+    <section className='p-4'>
+
+        {/* HEADING  */}
+        <div className='text-center ipadmini:text-left'>
+          <h1 className='font-extrabold text-2xl ipadmini:text-4xl '>{"Bookshelf"}</h1>
+          <p>{"Books and pieces of wisdom I enjoy reading"}</p>
+        </div>
+        {/* END------- HEADING */}
+
+        <section className='mt-10'>
+            <h1 className='font-bold'>Currently Under Development</h1>
+            <p>{"Check back soon :)"}</p>
+        </section>
+        </section>
+      
+      </div>
+    </div>
   )
 }
